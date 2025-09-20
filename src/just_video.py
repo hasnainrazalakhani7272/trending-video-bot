ef29111373507bd99f32b06a7aa512dcabda0668
hasnain7272 <104927265+hasnain7272@users.noreply.github.com>
2025-09-20 19:39:32 +0530

    Update just_video.py

--- src/just_video.py
+++ src/just_video.py
@@ -13,7 +13,7 @@ PEXELS_API_KEY = os.getenv('PEXELS_API_KEY')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-pro-latest")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # -------------------- Gemini Hashtag Generator --------------------
 def generate_hashtags_gemini(headline, summary):
